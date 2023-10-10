4e6c62cf7a64a4ecfb9f9068f5366c82d2efa8d1
saepasomba <saepasomba12@gmail.com>
2023-10-11 00:06:20 +0700

    Initial Commit

---
+++ Personal Atomic Project/Personal_Atomic_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Personal_Atomic_ProjectApp.swift
+//  Personal Atomic Project
+//
+//  Created by Sae Pasomba on 11/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Personal_Atomic_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
